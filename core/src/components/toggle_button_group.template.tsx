0eae920819a7a17d3744a5d61701060093a66bda
Yury Orlov <mpcteam.kvet@gmail.com>
2017-02-15 22:57:58 +0300

    Fix tempate

--- core/src/components/toggle_button_group.template.tsx
+++ core/src/components/toggle_button_group.template.tsx
@@ -1,11 +1,12 @@
 import { h } from '../definitions.template';
 
-let itemTemplate = ({ active, activate, item }) =>
+let itemTemplate = ({ active, activate, item }) => (
     <h.componentNode name="ToggleButton"
         props={{ 'active': active }}
         events={{ onClick: activate }}>
         <h.textNode content={item}/>
     </h.componentNode>
+)
 
 export let template = (
     <h.domNode tag="div">
